02b86a28272a369e7a1f176d32e145fc3eaa6aaf
GitBolt <saabisakhtar@gmail.com>
2025-12-30 09:57:42 -0600

    remove: copyright, update: footer logos

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from '@/styles/Footer.module.scss';
-import Twitter from '@/public/icons/twitter.svg';
+import XIcon from '@/public/icons/x.svg';
 import GitHub from '@/public/icons/github.svg';
 import LinkedIn from '@/public/icons/linkedin.svg';
 
@@ -12,17 +12,10 @@ export const Footer = function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.glassBar}>
-        <span>
-          SYED AABIS AKHTAR |
-          {' '}
-          {year}
-          {' '}
-          &#169;
-        </span>
         <div className={styles.icons}>
           <Link href="https://twitter.com/0xBolt" passHref legacyBehavior>
             <a target="_blank" rel="noopener noreferrer">
-              <Image src={Twitter} height={18} width={18} alt="Twitter" />
+              <Image src={XIcon} height={18} width={18} alt="X" />
             </a>
           </Link>
 
